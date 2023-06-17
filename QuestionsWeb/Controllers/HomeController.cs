51b606add42dcf0c8e9de1e7fc13e55f4c140f9f
Павел <shmachilin@gmail.com>
2023-06-17 15:15:08 +0300

    Работа с сервисами в контроллере

--- QuestionsWeb/Controllers/HomeController.cs
+++ QuestionsWeb/Controllers/HomeController.cs
@@ -4,18 +4,33 @@ namespace QuestionsWeb.Controllers;
 
 public class HomeController : Controller
 {
-    public HomeController()
+    private readonly ILogger<HomeController> _Logger;
+    private readonly IConfiguration _Config;
+
+    public HomeController(ILogger<HomeController> Logger, IConfiguration config)
     {
-        
+        _Logger = Logger;
+        _Config = config;
     }
 
     public IActionResult Index()
     {
+        _Logger.LogInformation("Вызов Index");
+
         return Content("Data from controller");
     }
 
     public IActionResult TestAction(int id)
     {
+        _Logger.LogInformation("Вызов TestAction c параметром {id}", id);
+
         return Content($"Data from test action {id}");
+    }
+
+    public IActionResult ConfigData(string Parameter = "TestString")
+    {
+        var value = _Config[Parameter];
+
+        return Content(value ?? "");
     }
 }

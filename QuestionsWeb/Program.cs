0446f689ac53447369aca8fb528ad12bc8e8d2a2
Павел <shmachilin@gmail.com>
2023-06-17 14:45:48 +0300

    Вывод простых текстовых сообщений в браузер

--- QuestionsWeb/Program.cs
+++ QuestionsWeb/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+//var str = app.Configuration["TestString"];
+
 app.MapGet("/", () => "Hello World!");
+
+//app.MapGet("Test", () => str);
+app.MapGet("Test", () => app.Configuration["TestString"]);
+
+/* --------------------------------------------------- */
 
 app.Run();

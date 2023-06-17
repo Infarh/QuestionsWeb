da135c9f77f9efcc798cb74e23c7d59bde901e46
Павел <shmachilin@gmail.com>
2023-06-17 15:03:06 +0300

    Передача параметров в контроллер из запроса

---
+++ QuestionsWeb/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuestionsWeb.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+        
+    }
+
+    public IActionResult Index()
+    {
+        return Content("Data from controller");
+    }
+
+    public IActionResult TestAction(int id)
+    {
+        return Content($"Data from test action {id}");
+    }
+}
--- QuestionsWeb/Program.cs
+++ QuestionsWeb/Program.cs
@@ -1,16 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddControllers(); // WebAPI на основе контроллеров
+builder.Services.AddControllersWithViews(); // Инфраструктура MVC = Контроллеры + представления (Razor)
+//builder.Services.AddRazorPages(); // Работа с веб-страницами
 
 /* --------------------------------------------------- */
 
 var app = builder.Build();
 
+app.UseRouting(); // Разбор маршрутов
+
 //var str = app.Configuration["TestString"];
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 //app.MapGet("Test", () => str);
 app.MapGet("Test", () => app.Configuration["TestString"]);
+
+//app.MapDefaultControllerRoute(); // Регистрация стандартного маршрута для MVC
+app.MapControllerRoute("default", "/{controller=Home}/{action=Index}/{id?}"); // Определение маршрута по умолчанию
 
 /* --------------------------------------------------- */
 

dbc5f75a75d0fdc73b22f74a8fa779cd6b03e468
Павел <shmachilin@gmail.com>
2023-06-17 16:00:14 +0300

    Ссылки между представлениями списка людей и отдельными карточками

---
+++ QuestionsWeb/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestionsWeb.Models;
+
+namespace QuestionsWeb.Controllers;
+
+public class PersonController : Controller
+{
+    private static List<Person> __Persons = new()
+    {
+        new() { Id = 1, LastName = "Иванов", Name = "Иван" },
+        new() { Id = 2, LastName = "Петров", Name = "Пётр" },
+        new() { Id = 3, LastName = "Сидоров", Name = "Сидор" },
+    };
+
+    public IActionResult Index()
+    {
+        return View(__Persons);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var person = __Persons.FirstOrDefault(x => x.Id == id);
+
+        if (person == null)
+            return NotFound();
+
+        return View(person);
+    }
+
+    //public IActionResult Create()
+    //{
+
+    //}
+
+    //public IActionResult Edit(int id)
+    //{
+
+    //}
+
+    //public IActionResult Delete(int id)
+    //{
+
+    //}
+}

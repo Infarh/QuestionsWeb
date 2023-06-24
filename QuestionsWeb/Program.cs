82dab99eb5cd8e643a06eca1b4decf1fcb3c5c82
Павел <shmachilin@gmail.com>
2023-06-24 14:26:19 +0300

    Удалерние хлама

--- QuestionsWeb/Program.cs
+++ QuestionsWeb/Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddControllers(); // WebAPI на основе контроллеров
 builder.Services.AddControllersWithViews(); // Инфраструктура MVC = Контроллеры + представления (Razor)
-//builder.Services.AddRazorPages(); // Работа с веб-страницами
 
 /* --------------------------------------------------- */
 
@@ -10,11 +8,8 @@ var app = builder.Build();
 
 app.UseRouting(); // Разбор маршрутов
 
-//var str = app.Configuration["TestString"];
 
-//app.MapGet("/", () => "Hello World!");
 
-//app.MapGet("Test", () => str);
 app.MapGet("Test", () => app.Configuration["TestString"]);
 
 //app.MapDefaultControllerRoute(); // Регистрация стандартного маршрута для MVC

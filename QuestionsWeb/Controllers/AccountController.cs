baf9c2ccb9c4378d24cd7a50e8fd31d6e02b0c3a
Павел <shmachilin@gmail.com>
2023-07-05 19:47:14 +0300

    Наследование макетов

---
+++ QuestionsWeb/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuestionsWeb.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _Logger;
+
+    public AccountController(ILogger<AccountController> Logger)
+    {
+        _Logger = Logger;
+    }
+
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    public IActionResult Login() => View();
+}

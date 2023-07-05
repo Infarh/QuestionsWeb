b7b75187cd84c87019832473e9c7bb62aeb72edf
Павел <shmachilin@gmail.com>
2023-07-05 20:35:36 +0300

    Доработка записи блога - добавление описание записи и картинки превью

--- QuestionsWeb.Domain/Entities/BlogPost.cs
+++ QuestionsWeb.Domain/Entities/BlogPost.cs
@@ -8,9 +8,15 @@ public class BlogPost : Entity
 
     public DateTimeOffset Time { get; set; } = DateTimeOffset.Now;
 
+    public required string AbstractText { get; set; }
+
     public required string Text { get; set; }
 
     public required int CategoryId { get; set; }
 
     public required int AuthorId { get; set; }
+
+    public required string PreviewImage { get; set; }
+
+    public required string MainImage { get; set; }
 }

217dc5c797b2c0e0df7177c87e46798765a5bd4a
Павел <shmachilin@gmail.com>
2023-07-05 20:24:00 +0300

    Категория блога

---
+++ QuestionsWeb.Domain/Entities/BlogCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace QuestionsWeb.Domain.Entities;
+
+public class BlogCategory
+{
+    public required int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public int? ParentId { get; set; }
+}
--- QuestionsWeb.Domain/Entities/BlogPost.cs
+++ QuestionsWeb.Domain/Entities/BlogPost.cs
@@ -2,13 +2,13 @@
 
 public class BlogPost
 {
-    public int Id { get; set; }
+    public required int Id { get; set; }
 
-    public string Title { get; set; } = null!;
+    public required string Title { get; set; }
 
     public DateTimeOffset Time { get; set; } = DateTimeOffset.Now;
 
-    public string Text { get; set; } = null!;
+    public required string Text { get; set; }
 
-
+    public required int CategoryId { get; set; }
 }

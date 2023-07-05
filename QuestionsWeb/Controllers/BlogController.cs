f6938424ab006287f3526196c8d8a90b8a189761
Павел <shmachilin@gmail.com>
2023-07-05 20:08:22 +0300

    Разделение представления списка блогов на частичные представления

---
+++ QuestionsWeb/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuestionsWeb.Controllers;
+
+public class BlogController : Controller
+{
+    private readonly ILogger<BlogController> _Logger;
+
+    public BlogController(ILogger<BlogController> Logger)
+    {
+        _Logger = Logger;
+    }
+
+    public IActionResult Index() => View();
+}

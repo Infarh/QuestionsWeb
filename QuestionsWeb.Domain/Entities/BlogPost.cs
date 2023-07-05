11b1380e04b18c77cd30611f632f642eaa262ef9
Павел <shmachilin@gmail.com>
2023-07-05 20:20:04 +0300

    Запись блога

---
+++ QuestionsWeb.Domain/Entities/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿namespace QuestionsWeb.Domain.Entities;
+
+public class BlogPost
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public DateTimeOffset Time { get; set; } = DateTimeOffset.Now;
+
+    public string Text { get; set; } = null!;
+
+
+}

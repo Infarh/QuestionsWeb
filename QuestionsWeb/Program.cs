fa64d50557bcf9ddda20f4f3a6c2e49253fa5618
Павел <shmachilin@gmail.com>
2023-07-15 16:18:02 +0300

    Добавление контекста БД в сервисы приложения

--- QuestionsWeb/Program.cs
+++ QuestionsWeb/Program.cs
@@ -1,10 +1,28 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using QRCoder;
+using QuestionsWeb.DAL.Context;
 using QuestionsWeb.Services;
 using QuestionsWeb.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); // Инфраструктура MVC = Контроллеры + представления (Razor)
+
+var db_connection_string = builder.Configuration.GetConnectionString("SQL");
+
+var db_string_builder = new SqlConnectionStringBuilder(db_connection_string);
+
+var user = builder.Configuration.GetSection("db")["User"];
+var pass = builder.Configuration.GetSection("db")["Password"];
+
+db_string_builder.UserID = user;
+db_string_builder.Password = pass;
+
+var edited_connection_string = db_string_builder.ConnectionString;
+//builder.Services.AddDbContext<QuestionsDB>(opt => opt.UseSqlServer(edited_connection_string));
+
+builder.Services.AddDbContext<QuestionsDB>(opt => opt.UseSqlServer(db_connection_string));
 
 builder.Services
     .AddTransient<IQRCodeService, QRCodeService>()

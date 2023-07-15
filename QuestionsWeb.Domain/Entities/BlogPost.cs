a5bfb0c779ede690b00c0e35ca3788c5fbeb976f
Павел <shmachilin@gmail.com>
2023-07-15 15:46:30 +0300

    Доработка сущности записи блога для добавления её в БД

--- QuestionsWeb.Domain/Entities/BlogPost.cs
+++ QuestionsWeb.Domain/Entities/BlogPost.cs
@@ -1,18 +1,27 @@
-﻿using QuestionsWeb.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using QuestionsWeb.Domain.Entities.Base;
 
 namespace QuestionsWeb.Domain.Entities;
 
 /// <summary> Blog post. </summary>
 public class BlogPost : Entity
 {
-
     /// <summary> Category id. </summary>
     public required int CategoryId { get; set; }
 
-    /// <summary> Post author. </summary>
+    /// <summary> Category </summary>
+    [ForeignKey(nameof(CategoryId))]
+    public BlogCategory Category { get; set; }
+
+    /// <summary> Post author Id </summary>
     public required int AuthorId { get; set; }
 
+    /// <summary> Post author </summary>
+    public Author Author { get; set; }
+
     /// <summary> Post title. </summary>
+    [MaxLength(255)]
     public required string Title { get; set; }
 
     /// <summary> Creation date. </summary>
@@ -22,12 +31,15 @@ public class BlogPost : Entity
     public DateTimeOffset? DateUpdate { get; set; }
 
     /// <summary> Post main image. </summary>
+    [MaxLength(500)]
     public required string MainImage { get; set; }
 
     /// <summary> Post content. </summary>
+    [MaxLength(10000)]
     public required string Content { get; set; }
 
     /// <summary> Abstract text. </summary>
+    [MaxLength(500)]
     public required string AbstractText { get; set; }
 
     /// <summary> Tags. </summary>
@@ -37,5 +49,6 @@ public class BlogPost : Entity
     public List<Review>? Reviews { get; set; }
 
     /// <summary> Preview image. </summary>
+    [MaxLength(500)]
     public required string PreviewImage { get; set; }
 }

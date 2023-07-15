fce8803bf201c4a29d31a81b2b12c07e245beb40
Павел <shmachilin@gmail.com>
2023-07-15 15:32:17 +0300

    Контекст базы данных

---
+++ QuestionsWeb.DAL/Context/QuestionsDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionsWeb.Domain.Entities;
+
+namespace QuestionsWeb.DAL.Context;
+
+public class QuestionsDB : DbContext
+{
+    public QuestionsDB(DbContextOptions<QuestionsDB> opt) : base(opt)
+    {
+        
+    }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+
+    public DbSet<BlogCategory> BlogCategories { get; set; }
+
+    public DbSet<Author> Authors { get; set; }
+}

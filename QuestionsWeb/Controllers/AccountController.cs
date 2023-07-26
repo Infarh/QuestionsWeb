707f71d9112bcacfa5e6f3f45ab6b9f4b201dbbe
Павел <shmachilin@gmail.com>
2023-07-26 21:10:45 +0300

    Процесс регистрации нового пользователя

--- QuestionsWeb/Controllers/AccountController.cs
+++ QuestionsWeb/Controllers/AccountController.cs
@@ -1,19 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuestionsWeb.Domain.Entities.Identity;
+using QuestionsWeb.ViewModels;
 
 namespace QuestionsWeb.Controllers;
 
 public class AccountController : Controller
 {
+    private readonly UserManager<User> _UserManager;
+    private readonly SignInManager<User> _SignInManager;
     private readonly ILogger<AccountController> _Logger;
 
-    public AccountController(ILogger<AccountController> Logger)
+    public AccountController(
+        UserManager<User> UserManager,
+        SignInManager<User> SignInManager,
+        ILogger<AccountController> Logger)
     {
+        _UserManager = UserManager;
+        _SignInManager = SignInManager;
         _Logger = Logger;
     }
 
-    public IActionResult SignUp()
+    public IActionResult SignUp() => View(new RegisterUserViewModel());
+
+    [HttpPost, ValidateAntiForgeryToken]
+    public async Task<IActionResult> SignUp(RegisterUserViewModel model)
     {
-        return View();
+        var user = new User
+        {
+            UserName = model.UserName,
+        };
+
+        var confirm_email_token = await _UserManager.GenerateEmailConfirmationTokenAsync(user);
+        var url = Url.Action("ConfirmEmail", new { token = confirm_email_token }); 
+
+        var creation_result = await _UserManager.CreateAsync(user, model.Password);
+        if (creation_result.Succeeded)
+        {
+            _Logger.LogInformation("Пользователь {user} успешно зарегистрирован", user.UserName);
+
+            await _UserManager.AddToRoleAsync(user, Role.Users);
+
+            await _SignInManager.SignInAsync(user, isPersistent: false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        foreach (var error in creation_result.Errors)
+        {
+            _Logger.LogWarning("Ошибка при регистрации пользователя {user}:{error}", user.UserName, error);
+            ModelState.AddModelError("", error.Description);
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> ConfirmEmail(string token)
+    {
+        //await _UserManager.ConfirmEmailAsync(User.Identity as User, token);
+        return RedirectToAction("Index", "Home");
     }
 
     public IActionResult Login() => View();

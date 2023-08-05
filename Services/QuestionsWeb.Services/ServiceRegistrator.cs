30b5168c5f94935957bc9c7938606937542f19e6
Павел <shmachilin@gmail.com>
2023-08-05 14:42:35 +0300

    Заготовка метода-расширения для регистрации сервисов, поддерживающая fluent-interface вызова

---
+++ Services/QuestionsWeb.Services/ServiceRegistrator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace QuestionsWeb.Services;
+
+public static class ServiceRegistrator
+{
+    public static IServiceCollection AddQuestionsWebServices(this IServiceCollection services)
+    {
+
+
+        return services;
+    }
+}
